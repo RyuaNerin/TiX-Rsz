7e4451f37fbdd0d61717b25b05a40691af81b4f5
RyuaNerin <admin@ryuar.in>
2020-09-17 10:56:34 +0900

    save

--- build.sh
+++ build.sh
@@ -1,22 +1,39 @@
-#!/bin/sh
+#!/bin/bash
 
 if ! [ -x "$(command -v docker)" ]; then
     echo "Please install docker"
     exit 1
 fi
 
+on_err() {
+    ERROR_CODE=$?
+    echo "error ${ERROR_CODE}"
+    echo "the command executing at the time of the error was"
+    echo "${BASH_COMMAND}"
+    echo "on line ${BASH_LINENO[0]}"
+    exit ${ERROR_CODE}
+}
+trap on_err ERR
+
 # Build libvips web x86_64 static
-git clone -b 'v8.10.1' --single-branch https://github.com/libvips/build-win64-mxe
-( \
-    cd build-win64-mxe; \
-    for i in ../patches/*.patch; do patch -l -p1 < "$i"; done; \
-    ./build.sh web x86_64 static \
-)
-unzip build-win64-mxe/build/*.zip -d . || exit 1
+if ! [ -d "vips-dev" ]; then
+    if ! [ -d "build-win64-mxe" ]; then
+        git clone -b 'v8.10.1' --single-branch https://github.com/libvips/build-win64-mxe
+    fi
 
-mv "$(find `pwd` -maxdepth 1 -type d -iname 'vips-dev-*')" vips-dev
+    ( \
+        cd build-win64-mxe; \
+        for i in ../patches/*.patch; do \
+            patch -l -p1 --forward < "$i" || true; \
+        done; \
+        ./build.sh web x86_64 static \
+    )
+    unzip build-win64-mxe/build/*.zip -d . || exit 1
 
-exit 1
+    mv "$(find `pwd` -maxdepth 1 -type d -iname 'vips-dev-*')" vips-dev
+fi
+
+exit 0
 
 ##################################################
 docker pull buildpack-deps:buster
