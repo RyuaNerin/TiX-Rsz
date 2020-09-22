6532efcb6935e35f1f26c99fd84620d14da9e41d
RyuaNerin <admin@ryuar.in>
2020-09-23 02:37:10 +0900

    save

--- build.sh
+++ build.sh
@@ -1,15 +1,29 @@
 #!/bin/sh
 
+on_err() {
+    ERROR_CODE=$?
+    echo
+    echo
+    echo "error ${ERROR_CODE}"
+    echo "the command executing at the time of the error was"
+    echo "${BASH_COMMAND}"
+    echo "on line ${BASH_LINENO[0]}"
+    exit ${ERROR_CODE}
+}
+trap on_err ERR
+
 . ./var.sh
 
-pacman -Sy --needed \
+pacman -Syq --needed --noconfirm \
     autoconf \
     automake-wrapper \
     git \
     libtool \
     make \
     patch \
+    wget \
     \
+    ${MINGW_PACKAGE_PREFIX}-cmake \
     ${MINGW_PACKAGE_PREFIX}-gcc \
     ${MINGW_PACKAGE_PREFIX}-gobject-introspection \
     ${MINGW_PACKAGE_PREFIX}-meson \
@@ -37,83 +51,156 @@ pacman -Sy --needed \
     ${MINGW_PACKAGE_PREFIX}-libwebp \
     ${MINGW_PACKAGE_PREFIX}-opencl-icd-git \
     ${MINGW_PACKAGE_PREFIX}-openexr \
-    ${MINGW_PACKAGE_PREFIX}-orc \
     ${MINGW_PACKAGE_PREFIX}-pango \
     ${MINGW_PACKAGE_PREFIX}-pkg-config \
-    ${MINGW_PACKAGE_PREFIX}-poppler \
     ${MINGW_PACKAGE_PREFIX}-zlib
 
 # clear
-rm -rf "${BUILD_DIR}"
-mkdir -p "${BUILD_DIR}/bin"
-mkdir -p "${BUILD_DIR}/src"
+#rm -rf "${BUILD_DIR}"
+mkdir -p "${BUILD_DIR}/bin" || true
+mkdir -p "${BUILD_DIR}/src" || true
 
 ####################################################################################################
 # orc
-cd "${BUILD_DIR}/src"
+ORC_TAR_NAME=$(basename ${ORC_TAR})
+ORC_DIR=$(basename ${ORC_TAR} ${ORC_EXT})
+if [ ! -f "${BUILD_DIR}/b-${ORC_DIR}" ]; then
+    cd "${BUILD_DIR}/src"
 
-wget "${ORC_TAR}"
-echo "${ORC_TAR_HASH} *${ORC_TAR_NAME}" | sha256sum -c -
-tar Jxf "${ORC_TAR_NAME}" -C "${BUILD_DIR}/src"
+    rm -rf "${ORC_TAR_NAME}" "${ORC_DIR}" "${ORC_DIR}-build" || true
 
-cd "${BUILD_DIR}/src/${ORC_DIR}"
-for i in ${BASE_DIR}/patches/orc*.patch; do
-    patch -l -p1 --forward < "$i"
-done
-${MINGW_PREFIX}/bin/meson \
-    --buildtype=release \
-    --strip \
-    --libdir='lib' \
-    --includedir='include' \
-    -Ddefault_library=static \
-    -Dbenchmarks=disabled \
-    -Dexamples=disabled \
-    -Dgtk_doc=disabled \
-    -Dtests=disabled \
-    -Dauto_features=enabled \
-    "${BUILD_DIR}/src/${ORC_DIR}-build"
+    wget "${ORC_TAR}"
+    echo "${ORC_TAR_HASH} *$(basename ${ORC_TAR})" | sha256sum -c -
+    tar Jxf "${ORC_TAR_NAME}" -C "${BUILD_DIR}/src"
 
-cd "${BUILD_DIR}/src/${ORC_DIR}-build"
-ninja
-DESTDIR="${BUILD_DIR}" ninja install
+    cd "${BUILD_DIR}/src/${ORC_DIR}"
+    for i in ${BASE_DIR}/patches/orc*.patch; do
+        patch -p1 < "$i"
+    done
+    ${MINGW_PREFIX}/bin/meson \
+        --buildtype=release \
+        --strip \
+        --prefix "${BUILD_DIR}" \
+        --libdir='lib' \
+        --includedir='include' \
+        -Ddefault_library=static \
+        -Dbenchmarks=disabled \
+        -Dexamples=disabled \
+        -Dgtk_doc=disabled \
+        -Dtests=disabled \
+        -Dauto_features=enabled \
+        "${BUILD_DIR}/src/${ORC_DIR}-build"
+
+    cd "${BUILD_DIR}/src/${ORC_DIR}-build"
+    ninja
+    ninja install
+
+    touch "${BUILD_DIR}/b-${ORC_DIR}"
+    exit
+fi
+
+####################################################################################################
+# poppler-glib
+#<<'EOF'
+POPPLER_TAR_NAME=$(basename ${POPPLER_TAR})
+POPPLER_DIR=$(basename ${POPPLER_TAR} ${POPPLER_EXT})
+if [ ! -f "${BUILD_DIR}/b-${POPPLER_DIR}" ]; then
+    cd "${BUILD_DIR}/src"
+
+    rm -rf "${POPPLER_TAR_NAME}" "${POPPLER_DIR}" "${POPPLER_DIR}-build" || true
+
+    wget "${POPPLER_TAR}"
+    echo "${POPPLER_TAR_HASH} *$(basename ${POPPLER_TAR})" | sha256sum -c -
+    tar Jxf "${POPPLER_TAR_NAME}" -C "${BUILD_DIR}/src"
+
+    cd "${BUILD_DIR}/src/${POPPLER_DIR}"
+    for i in ${BASE_DIR}/patches/poppler*.patch; do
+        patch -p1 < "$i"
+    done
+
+    mkdir "${BUILD_DIR}/src/${POPPLER_DIR}-build"
+    cd "${BUILD_DIR}/src/${POPPLER_DIR}-build"
+
+    "${MINGW_PREFIX}/bin/cmake.exe" \
+        -Wno-dev \
+        -G"MSYS Makefiles" \
+        -DCMAKE_BUILD_TYPE=release \
+        -DCMAKE_INSTALL_PREFIX=${BUILD_DIR} \
+        -DBUILD_SHARED_LIBS=OFF \
+        -DBUILD_CPP_TESTS=OFF \
+        -DBUILD_GTK_TESTS=OFF \
+        -DBUILD_QT5_TESTS=OFF \
+        -DENABLE_CMS=lcms2 \
+        -DENABLE_CPP=ON \
+        -DENABLE_DCTDECODER=libjpeg \
+        -DENABLE_GLIB=ON \
+        -DENABLE_GOBJECT_INTROSPECTION=ON \
+        -DENABLE_GTK_DOC=OFF \
+        -DENABLE_LIBCURL=OFF \
+        -DENABLE_LIBOPENJPEG=openjpeg2 \
+        -DENABLE_LIBPNG=ON \
+        -DENABLE_LIBTIFF=ON \
+        -DENABLE_NSS3=ON \
+        -DENABLE_QT5=OFF \
+        -DENABLE_UNSTABLE_API_ABI_HEADERS=ON \
+        -DENABLE_UTILS=OFF \
+        -DENABLE_ZLIB_UNCOMPRESS=OFF \
+        -DENABLE_ZLIB=ON \
+        -DSPLASH_CMYK=ON \
+        "${BUILD_DIR}/src/${POPPLER_DIR}"
+    CC=${CC} make ${MAKE_JOBS}
+    make install
+
+    touch "${BUILD_DIR}/b-${POPPLER_DIR}"
+    exit
+fi
+#EOF
 
 ####################################################################################################
 # libvips
-cd "${BUILD_DIR}/src"
+VIPS_TAR_NAME=$(basename ${VIPS_TAR})
+VIPS_DIR=$(basename ${VIPS_TAR} ${VIPS_EXT})
+if [ ! -f "${BUILD_DIR}/b-${VIPS_DIR}" ]; then
+    cd "${BUILD_DIR}/src"
 
-wget "${VIPS_TAR}"
-echo "${VIPS_HASH} *${VIPS_TAR_NAME}" | sha256sum -c -
-tar xzf "${VIPS_TAR_NAME}" -C "${BUILD_DIR}/src"
+    rm -rf "${VIPS_TAR_NAME}" "${VIPS_DIR}" "${VIPS_DIR}-build" || true
 
-cd "${BUILD_DIR}/src/${VIPS_DIR}"
-for i in ${BASE_DIR}/patches/libvips*.patch; do
-    patch -l -p1 --forward < "$i"
-done
-./configure \
-    --prefix="${BUILD_DIR}" \
-    --build=${MINGW_CHOST} \
-    --host=${MINGW_CHOST} \
-    --target=${MINGW_CHOST} \
-    --enable-static \
-    --disable-shared \
-    --enable-debug=no \
-    --disable-introspection \
-    --disable-deprecated \
-    --without-openslide \
-    --without-pdfium \
-    --without-cfitsio \
-    --without-OpenEXR \
-    --without-nifti \
-    --without-matio \
-    --without-ppm \
-    --without-analyze \
-    --without-radiance \
-    --without-imagequant \
-    --without_poppler \
-    lt_cv_deplibs_check_method="pass_all"
+    wget "${VIPS_TAR}"
+    echo "${VIPS_TAR_HASH} *$(basename ${VIPS_TAR})" | sha256sum -c -
+    tar xzf "${VIPS_TAR_NAME}" -C "${BUILD_DIR}/src"
 
-make ${MAKE_JOBS}
-make install
+    cd "${BUILD_DIR}/src/${VIPS_DIR}"
+    for i in ${BASE_DIR}/patches/libvips*.patch; do
+        patch -p1 < "$i"
+    done
+    ./configure \
+        --prefix="${BUILD_DIR}" \
+        --build=${MINGW_CHOST} \
+        --host=${MINGW_CHOST} \
+        --target=${MINGW_CHOST} \
+        --enable-static \
+        --disable-shared \
+        --enable-debug=no \
+        --disable-introspection \
+        --disable-deprecated \
+        --without-openslide \
+        --without-pdfium \
+        --without-cfitsio \
+        --without-OpenEXR \
+        --without-nifti \
+        --without-matio \
+        --without-ppm \
+        --without-analyze \
+        --without-radiance \
+        --without-imagequant \
+        lt_cv_deplibs_check_method="pass_all"
+
+    make ${MAKE_JOBS}
+    make install
+
+    touch "${BUILD_DIR}/b-${VIPS_DIR}"
+    exit
+fi
 
 ####################################################################################################
 
--- var.sh
+++ var.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-MAKE_JOBS="-j 8"
+MAKE_JOBS="-j 4"
 
 export BASE_DIR="${PWD}"
 export BUILD_DIR="${PWD}/build"
 
 export MINGW_PREFIX=/mingw64
+
+export CC="${MINGW_PREFIX}/bin/gcc.exe"
+export CXX="${MINGW_PREFIX}/bin/g++.exe"
 
 export PATH=${MINGW_PREFIX}/bin:${PATH}
 export ACLOCAL_PATH=${MINGW_PREFIX}/share/aclocal:/usr/share/aclocal
@@ -15,14 +18,18 @@ export CONFIG_SITE=${MINGW_PREFIX}/etc/config.site
 export MINGW_CHOST=x86_64-w64-mingw32
 export MINGW_PACKAGE_PREFIX=mingw-w64-x86_64
 
-export PKG_CONFIG_PATH=${BUILD_DIR}/lib/pkgconfig:${MINGW_PREFIX}/lib/pkgconfig:${MINGW_PREFIX}/share/pkgconfig
+export PKG_CONFIG_PATH=${BUILD_DIR}/lib/pkgconfig:${BUILD_DIR}/share/lib/pkgconfig:${MINGW_PREFIX}/lib/pkgconfig:${MINGW_PREFIX}/share/pkgconfig
 
 VIPS_TAR="https://github.com/libvips/libvips/releases/download/v8.10.1/vips-8.10.1.tar.gz"
-VIPS_TAR_NAME="vips-8.10.1.tar.gz"
 VIPS_TAR_HASH="e089bb4f73e1dce866ae53e25604ea4f94535488a45bb4f633032e1d2f4e2dda"
-VIPS_DIR="vips-8.10.1"
+VIPS_EXT=".tar.gz"
 
 ORC_TAR="https://gstreamer.freedesktop.org/src/orc/orc-0.4.31.tar.xz"
-ORC_TAR_NAME="orc-0.4.31.tar.xz"
 ORC_TAR_HASH="a0ab5f10a6a9ae7c3a6b4218246564c3bf00d657cbdf587e6d34ec3ef0616075"
-ORC_DIR="orc-0.4.31"
+ORC_EXT=".tar.xz"
+
+POPPLER_TAR="https://poppler.freedesktop.org/poppler-0.88.0.tar.xz"
+POPPLER_TAR_HASH="b4453804e9a5a519e6ceee0ac8f5efc229e3b0bf70419263c239124474d256c7"
+POPPLER_EXT=".tar.xz"
+
+alias wget='wget -q --show-progress'
